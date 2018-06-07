f2e0f0f623947312b1874443fdd9f8634d66cdb9
s164930 <s164930@student.dtu.dk>
2018-06-07 10:03:22 +0200

    Add files via upload
    Har ændret til 720p kamera, man skal give afstanden mellem de to firkanter på QR-koden i pixels som argument, når man kalder scriptet, så får man afstanden i cm fra QR-koden

--- beregnAfstand.py
+++ beregnAfstand.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 import sys
 
-FLength = 590 # udregnet gennem tingene der står i "Den_nemme.txt", 
+FLength = 1015 # udregnet gennem tingene der står i "Den_nemme.txt", 
 	      # kræver at man har taget et billede, hvor man kender afstanden til objektet
 	      # til en initial udregning.
-realWidth = float(sys.argv[1])
-pixelWidth = float(sys.argv[2])
+realWidth = 5.11811024
+pixelWidth = float(sys.argv[1])
 
 Distance = (realWidth * FLength)/pixelWidth #udregner længden fra objektet
 
-print"Du er", Distance, "cm fra objektet"
+print "Du er", Distance, "cm fra objektet"
